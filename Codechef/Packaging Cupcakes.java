8fbb1e1cb0840cea33892c5ef64cb4e787ab160c
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-23 12:53:27 +0530

    codechef

---
+++ Codechef/Packaging Cupcakes.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int t= sc.nextInt();
+		for(int i=0; i<t; i++){
+		    int n= sc.nextInt();
+		    System.out.println(n/2+1);
+		}
+	}
+}
---
+++ Codechef/Small Factorial.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static int fact(int n){
+        if(n==0 || n==1){
+            return 1;
+        }   
+        return n*fact(n-1);
+    }
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int t= sc.nextInt();
+		for(int i=0; i<t; i++){
+		    int n= sc.nextInt();
+		    System.out.println(fact(n));
+		}
+	}
+}
