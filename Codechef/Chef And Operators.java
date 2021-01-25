7ceee7308d360cf262dded82a9ce40ed93c1c040
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2021-01-25 13:49:14 +0530

    codechef

---
+++ Codechef/Chef And Operators.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int t= sc.nextInt();
+		for(int i=0; i<t; i++){
+		    int a= sc.nextInt();
+		    int b= sc.nextInt();
+		    if(a>b){
+		        System.out.println(">");
+		    }else if(a<b){
+		        System.out.println("<");
+		    }else{
+		        System.out.println("=");
+		    }
+		}
+	}
+}
