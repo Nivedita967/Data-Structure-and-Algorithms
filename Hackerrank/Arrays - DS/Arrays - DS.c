3d176868e873780361fa4acfee85318b48c9fcf7
Yashasvi <kmryashasvi@gmail.com>
2021-02-09 19:25:15 +0530

    fix: Added hackerrank problem
    reason for deleting keep - it is of zero byte and no significance in the repo
    
    reason for deleting problem1 and solution.c - it is the helloword program whose solution already exists in the repository in the same language
    
    reason for adding array problems - Mera Dil Bahut Bada Hai

---
+++ Hackerrank/Arrays - DS/Arrays - DS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    //variable declaration
+    int t = 0;
+    int a[1000];
+    scanf("%d ", &t);//getting user input
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d", &a[i]);// getting the array
+    }
+    for (int p = 0; p < t; p++)
+    {
+        printf("%d ",a[t - p - 1]);// printing the array
+    }
+}
