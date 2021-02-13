26abdad7f0dc7cb324d8cdb7ceff317eb3ccf4fa
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 13:12:49 +0530

    #31 added comments to helping chef.java

--- Codechef/Helping Chef/Helping Chef.java
+++ Codechef/Helping Chef/Helping Chef.java
@@ -1,5 +1,7 @@
+//TODO: Printing strings for specific inputs
 /* package codechef; // don't place package name! */
 
+//TODO: Importing java packages
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -7,12 +9,12 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef {
 	public static void main(String[] args) throws java.lang.Exception {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);   //TODO: getting user input
 		int t = sc.nextInt();
 		for (int i = 0; i < t; i++) {
 			int n = sc.nextInt();
 			if (n < 10) {
-				System.out.println("Thanks for helping Chef!");
+				System.out.println("Thanks for helping Chef!");   //TODO: Printing strings
 			} else {
 				System.out.println(-1);
 			}
