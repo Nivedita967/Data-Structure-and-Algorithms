5d7bbdbca7e7a6fba113aed161793a62af814a9d
Mahindra-7 <smr210898@gmail.com>
2021-02-13 20:31:08 +0530

    XOR operation

--- Leetcode/XOR operation/Xoroperation.java.java
+++ Leetcode/XOR operation/Xoroperation.java.java
@@ -1,7 +1,11 @@
+import java.io.*;
 class Xoroperation {
     public int xorOperation(int n, int start) {
-        
+        // Function to find the XOR of 
+    // all elements in the array 
         int[] ar = new int[n];
+     // Iterating through every element in 
+        // the array
         for(int i = 0; i < n; i++) {
             ar[i] = start + 2*i;
         }
@@ -10,6 +14,7 @@ class Xoroperation {
         for(int i = 0; i < n-1; i++) {
             result = result ^ ar[i+1];
         }
+    //Return the XOR 
         return result;  
     }
 }
