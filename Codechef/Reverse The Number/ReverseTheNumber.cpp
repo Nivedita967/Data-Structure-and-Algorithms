5ade7a3339bcda082d0d62ee7207d19b45c39036
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 14:48:21 +0530

    #31 added comments to ReverseTheNumber.cpp

--- Codechef/Reverse The Number/ReverseTheNumber.cpp
+++ Codechef/Reverse The Number/ReverseTheNumber.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+//TODO: Reverse the digits of a given number
+
+#include <iostream>                                      //TODO: Including header file
 using namespace std;
 
 int main(void)
@@ -6,16 +8,16 @@ int main(void)
     int t;
     cin >> t;
     int mul;
-    for (int i = 0; i < t; i++)
+    for (int i = 0; i < t; i++)                         //TODO: Getting user input
     {
         int num = 0;
         int num2 = 0;
         cin >> num;
         while (num > 0)
         {
-            num2 = num2 * 10 + (num % 10);
+            num2 = num2 * 10 + (num % 10);              //TODO: Reversing the given digit
             num = (num - (num % 10)) / 10;
-        }
-        cout << num2 << endl;
+        } 
+        cout << num2 << endl;                           //TODO: Printing result
     }
 }
