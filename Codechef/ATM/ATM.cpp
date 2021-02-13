429b540d436e52a54a2c11c4977b760feb3469e5
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 08:42:56 +0530

    Create ATM.cpp

---
+++ Codechef/ATM/ATM.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) 
+{
+    
+    int a;
+    float b;
+    scanf("%d %f",&a,&b);
+    if(a%5==0 && a+0.5<=b)
+        printf("%.2f",b-a-0.5);
+    else
+        printf("%.2f",b);
+        
+	return 0;
+}
