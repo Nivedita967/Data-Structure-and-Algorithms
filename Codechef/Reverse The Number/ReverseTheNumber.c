458e129f4bf38e9281ed0858be54ce136b9a1f7f
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 14:45:12 +0530

    #31 added comments to ReverseTheNumber.c

--- Codechef/Reverse The Number/ReverseTheNumber.c
+++ Codechef/Reverse The Number/ReverseTheNumber.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+//TODO: Reverse the digits of a given number
+
+#include <stdio.h>                                                //TODO: Including header files
 
 int main(void)
 {
     int t;
-    scanf("%d", &t);
+    scanf("%d", &t);                                             //TODO: Getting user input
     int mul;
     for (int i = 0; i < t; i++)
     {
@@ -12,9 +14,9 @@ int main(void)
         scanf("%d", &num);
         while (num > 0)
         {
-            num2 = num2 * 10 + (num % 10);
+            num2 = num2 * 10 + (num % 10);                        //TODO: Reversing the given number
             num = (num - (num % 10)) / 10;
         }
-        printf("%d\n", num2);
+        printf("%d\n", num2);                                     //TODO: Printing the result
     }
 }
