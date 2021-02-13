238b5f17810ad7be0cc0539da02c8c8bd1a8b24a
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 13:21:34 +0530

    #31 added comments to luckyfour.java

--- Codechef/Lucky Four/Lucky Four.java
+++ Codechef/Lucky Four/Lucky Four.java
@@ -1,23 +1,27 @@
+//TODO: Print number of occurences of the digit 4 in the respective integer from Kostya's list.
 /* package codechef; // don't place package name! */
 
+//TODO: Importing java packages
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 class Codechef {
 	public static void main(String[] args) throws java.lang.Exception {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);   //TODO: Taking user input
 		int t = sc.nextInt();
 		for (int i = 0; i < t; ++i) {
 			int n = sc.nextInt();
 			int count = 0;
+			
+			//TODO: Calculating answer
 			while (n > 0) {
 				if (n % 10 == 4) {
 					++count;
 				}
 				n = n / 10;
 			}
-			System.out.println(count);
+			System.out.println(count);   //TODO: Printing answer
 		}
 	}
 }
