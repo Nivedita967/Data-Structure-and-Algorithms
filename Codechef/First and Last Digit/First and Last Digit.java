d547d10290c871633e510d706316fc2daeb41514
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 13:10:00 +0530

    #31 added comments to  firstandlastdigit.java

--- Codechef/First and Last Digit/First and Last Digit.java
+++ Codechef/First and Last Digit/First and Last Digit.java
@@ -1,5 +1,7 @@
+//TODO: display the sum of first and last digits of N in a new line.
 /* package codechef; // don't place package name! */
 
+//TODO: Importing java packages
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -9,11 +11,11 @@ class Codechef {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		for (int i = 0; i < t; i++) {
-			int n = sc.nextInt();
+			int n = sc.nextInt();   //TODO: Getting user input 
 			int digits = (int) Math.log10(n);
-			int first = (int) (n / Math.pow(10, digits));
+			int first = (int) (n / Math.pow(10, digits));   //TODO: calculating answer
 			int last = n % 10;
-			System.out.println(first + last);
+			System.out.println(first + last);   //TODO: printing answer
 		}
 	}
 }
