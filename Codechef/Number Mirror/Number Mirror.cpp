205951839a8295ad10c0c75e3fb6fd3f84ee0d3c
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 17:38:13 +0530

    #71 Added numbermirror.cpp

---
+++ Codechef/Number Mirror/Number Mirror.cpp
@@ -0,0 +1,14 @@
+//TODO: To get a number from user and print it
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) 
+{    
+    int x;
+    cin>>x;          //TODO: getting input from user
+    cout>>x;         //TODO: displaying the same integer
+}
+
+
