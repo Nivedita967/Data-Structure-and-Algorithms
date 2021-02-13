0cec7568701be2cbaa8ec2a9f1dddf916fad8c63
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-13 13:28:33 +0530

    #31 added comments to packaging cupcakes.java

--- Codechef/Packaging Cupcakes/Packaging Cupcakes.java
+++ Codechef/Packaging Cupcakes/Packaging Cupcakes.java
@@ -1,5 +1,7 @@
+//TODO: output the package size that will maximize the number of leftover cupcakes.
 /* package codechef; // don't place package name! */
 
+//TODO: Importing java packages
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -7,11 +9,11 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef {
 	public static void main(String[] args) throws java.lang.Exception {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);   //TODO: Getting user input
 		int t = sc.nextInt();
 		for (int i = 0; i < t; i++) {
 			int n = sc.nextInt();
-			System.out.println(n / 2 + 1);
+			System.out.println(n / 2 + 1);   //TODO: Calculating answer and printing it
 		}
 	}
 }
