1b3893668334819094358f69412daec7de272b19
kavania2002 <72624080+kavania2002@users.noreply.github.com>
2021-02-14 13:53:13 +0530

    Added comments for better explanantion

--- Hackerrank/Day of The Programmer/dayOfTheProgrammer.py
+++ Hackerrank/Day of The Programmer/dayOfTheProgrammer.py
@@ -8,14 +8,18 @@ import sys
 
 # Complete the dayOfProgrammer function below.
 def dayOfProgrammer(year):
+    # For Gregorian  (Leap)               # For Julian (Leap)
     if (leapyear(year) and year>1918) or (year%4==0 and year<1918):
         return '12.09.'+str(year)
     else:
+        # 1918 : Start of Gregorian and end of Julian Calendar
         if year==1918:
             return '26.09.'+str(year)
         else:
             return '13.09.'+str(year)
-        
+
+# Function regarding to check whether it is leap year or not.
+# Leap years are either divisible by 400    or     divisible by 4 but not by 100
 def leapyear(year):
     if year%4==0:
         if year%100==0:
@@ -28,7 +32,7 @@ def leapyear(year):
     else:
         False
 
-
+# Internal HackerRank code (already given)
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
