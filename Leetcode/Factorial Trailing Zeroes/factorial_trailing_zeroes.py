8918ee5af60df4679d3fbcdbe3c6c99aeba7fac0
gopichandpuli9 <64889409+gopichandpuli9@users.noreply.github.com>
2021-02-14 14:15:23 +0530

    Update factorial_trailing_zeroes.py

--- Leetcode/Factorial Trailing Zeroes/factorial_trailing_zeroes.py
+++ Leetcode/Factorial Trailing Zeroes/factorial_trailing_zeroes.py
@@ -3,4 +3,4 @@ class Solution:
         if n < 5:
             return 0 #for all numbers less than 5, the number of trailing zeroes is 0
         else:
-            return n//5+self.trailingZeroes(n//5) #for any number greater than 5, we need to implement this function recursively till it becomes less than 5
+            return n//5+self.trailingZeroes(n//5) #for any number greater than 5, we need to implement this function recursively till its value becomes less than 5
