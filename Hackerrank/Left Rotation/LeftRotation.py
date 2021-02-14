d601414db34773618b885e40901a9c2c5fd23cb5
saksham-s1ngh <saksham249singh@gmail.com>
2021-02-14 15:49:46 +0530

    made a few edits to the Left Rotation problem in Python

--- Hackerrank/Left Rotation/LeftRotation.py
+++ Hackerrank/Left Rotation/LeftRotation.py
@@ -1,3 +1,5 @@
+#Working of rotateLeft: We append the first element to the list, and then remove it form the beginning
+# thus, left rotating the array
 def rotateLeft(d, elements):
     for i in range(d):
         elements.append(elements[0])
@@ -9,8 +11,11 @@ inputs = input("Enter number of elements, and no. of rotations: ")
 inputs = inputs.split()
 elNum = int(inputs[0])
 rotations = int(inputs[1])
+
+#we use map to split the input string and convert each split numeric character to integer simultaneously
+#we need to use list, to store each int as list input as without it map just returns a map object
 elements = list(map(int, input("Enter array elements: ").split()))
 
-result = rotateLeft(rotations, list(set(elements)))
+result = rotateLeft(rotations, elements)
 
 print(result)
