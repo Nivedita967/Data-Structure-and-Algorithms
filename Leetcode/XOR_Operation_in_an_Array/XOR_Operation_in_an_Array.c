60a22c347e12c7bc4e2b7ed36041d23fb6aed08b
TechSpiritSS <76687985+TechSpiritSS@users.noreply.github.com>
2021-02-14 13:26:19 +0530

    Added Comments

--- Leetcode/XOR_Operation_in_an_Array/XOR_Operation_in_an_Array.c
+++ Leetcode/XOR_Operation_in_an_Array/XOR_Operation_in_an_Array.c
@@ -1,10 +1,9 @@
-//issue #25
 int xorOperation(int n, int start)
 {
     int sum=0;
-    for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++) //Loop for taking gaps
     {
-        sum ^= (start + 2*i);
+        sum ^= (start + 2*i); // Taking XOR of the next number which is an AP of 2 from start and adding it to previous sum
     }
     return sum;
 }
