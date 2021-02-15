7340ffce2edcdfe24260d83d4b0d325447860218
Simonne Letetia Pinto <53074235+simonnepinto@users.noreply.github.com>
2021-02-15 10:16:26 +0530

    Update primeTuples.java

--- Codechef/Prime Tuples/primeTuples.java
+++ Codechef/Prime Tuples/primeTuples.java
@@ -1,47 +1,60 @@
+// Link to the problem : https://www.codechef.com/problems/PTUPLES
+
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-class primeTuples
-{
+public class Main{
+    public static int n = 1000001;
+    public static boolean prime[]=new boolean[n+1]; 
+    public static int answer[]=new int[n];
     
-    static void sieve(int n, boolean prime[]) //Finding prime numbers using Sieve of Eratosthenes
+    static void sieve() //Finding prime numbers using Sieve of Eratosthenes
     { 
+        Arrays.fill(prime,true);  //initialise the entries as true
+        Arrays.fill(answer,0);    //initialise the entries as 0
+        
+        prime[0]=false;
+        prime[1]=false;
+        
         for (int p = 2; p * p <= n; p++) { 
-      
             if (prime[p] == true) { 
                 // Update all multiples of p 
                 for (int i = p * p; i <= n; i += p) 
-                    prime[i] = false; 
+                    prime[i] = false; // compute the prime numbers
             } 
-        } 
-    } 
-    
-    static void primeTuples(int n) 
-    { 
-        // Finding all primes from 1 to n 
-        boolean prime[]=new boolean[n + 1]; 
-        Arrays.fill(prime,true); //initialise the entries as true
-        sieve(n, prime); 
+        }
+        
         
         int count = 0;
-          
-        for (int i = 3; i <= n-2; ++i) { 
-            if (prime[2] && prime[i] && prime[i + 2])  // Compute the prime tuple values
+        for (int i = 5; i < n; ++i) { 
+            if (prime[i] && prime[i - 2]){ //determine prime tuples
                 count++; 
+            }
+            answer[i] = count;  
         } 
-        System.out.println(count); // Print result
+    
     } 
     
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine()); // take in the number of entries
-		
-		for(int i=0; i<n; ++i){
-		    int a = Integer.parseInt(br.readLine()); // take in the values
-		    primeTuples(a);
+    public static void main (String[] args) throws java.lang.Exception{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	PrintWriter pw = new PrintWriter(System.out);
+	try{
+		int a;
+		sieve();
+		int n = Integer.parseInt(br.readLine()); //number of entries
+		for(int i=0; i<n; i++){
+			a = Integer.parseInt(br.readLine());
+			if(a < 5){
+			    System.out.println("0");
+			    continue;
+			}
+			pw.println(answer[a]); //print the number of possible prime tuples
 		}
+		pw.flush();
+		pw.close();
 	}
+        catch(Exception ex){
+
+        }
+    }
 }
