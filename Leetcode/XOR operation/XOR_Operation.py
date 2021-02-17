0124b41f847c18292d51621e18b5361e42db89c3
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2021-02-16 20:24:39 -0800

    Update_Xor_Operation

--- Leetcode/XOR operation/XOR_Operation.py
+++ Leetcode/XOR operation/XOR_Operation.py
@@ -1,10 +1,11 @@
 from functools import reduce
-def xorOperation(self, n: int, start: int) -> int:
-        nums=[] #list to store numbers based on the given condition
-        for i in range(n): #iterates from 0 to n-1
-            k=start+(2*i)
-            nums.append(k) #adding every k at the end of num 
+class Solution:
+    def xorOperation(self,n: int, start: int) -> int:
+        nums=[]  #list to store numbers based on the given condition
+        for i in range(n):  #iterates from 0 to n-1
+            m=start+(2*i)
+            nums.append(m) #adding every k at the end of num 
             
-        res = reduce(lambda x, y: x ^ y, nums) #applying Xor operation and calculates like((((0^2)^4)^6)^8). 
-        print(res)
+        res = reduce(lambda x, y: x ^ y, nums) #applying Xor operation and calculates like((((0^2)^4)^6)^8).
+        return(res)
  
