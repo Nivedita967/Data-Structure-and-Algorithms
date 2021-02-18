1c3e8876920c06b1950b44b89bf4317d0edd915b
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-18 14:36:40 +0530

    Update Helping Chef.c

--- Codechef/Helping Chef/Helping Chef.c
+++ Codechef/Helping Chef/Helping Chef.c
@@ -1,21 +1,23 @@
+//TODO: Display string if input is less than 10
+
 #include <stdio.h>
 
 
 int main(void) 
 {
     int t;
-    scanf("%d",&t);
+    scanf("%d",&t);             //TODO:Get never of test cases
     for(int i = 0; i < t;i++)
     {   int n;
-        scanf("%d", &n);
+        scanf("%d", &n);        //TODO:Get input
         if(n < 10)
         {
-            printf("Thanks for helping Chef!\n");
+            printf("Thanks for helping Chef!\n");  
 
         }
         else
         {
-            printf("-1\n");
+            printf("-1\n");      //TODO: Printing results
         }
 
     }
