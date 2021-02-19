d048e3a6e52b860b955868070bb455888da41a48
sloth-panda <achyutpanda488@gmail.com>
2021-02-19 22:34:07 +0530

    added finding local minima in an array

---
+++ GeeksForGeeks/Local-Minima-In-an-Array/local_minima_in_an_array.java
@@ -0,0 +1,23 @@
+public class LocalMinima {
+ 
+ public int findLocalMinima(int [] arr, int start, int end){
+ 
+ /*find the mid index*/
+ int mid = start + (end-start)/2;
+ int size = arr.length;
+ 
+ /*check the local minima 
+ *first check if left and right neighbor exists */
+ if((mid==0 || arr[mid-1]>arr[mid]) &&
+ (mid==size-1 || arr[mid+1]>arr[mid]))
+ return arr[mid];
+ /* check if left neighbor is less than mid element, if yes go left */
+ else if(mid>0 && arr[mid]>arr[mid-1]){
+ return findLocalMinima(arr, start, mid);
+ }
+ else { 
+ /*check if right neighbor is greater than mid element, if yes go right */
+ return findLocalMinima(arr, mid+1, end);
+ }
+ }
+ 
