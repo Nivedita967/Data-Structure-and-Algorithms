bd7df9b0fd7d9aedc271b9d4d79fd00612c89e91
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-21 15:41:52 +0530

    Update Find The Running Median.c

--- Hackerrank/Find the Running Median/Find The Running Median.c
+++ Hackerrank/Find the Running Median/Find The Running Median.c
@@ -1,3 +1,5 @@
+//TODO: Finding median of given array
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -5,12 +7,12 @@
 int main()
 {
     int t = 0;
-    scanf("%d", &t);
+    scanf("%d", &t);                                      //TODO: Reading number of test cases
     int arr[t];
 
     for (int i = 0; i < t; i++)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d", &arr[i]);                            //TODO: Reading elements of array
         float med = 0;
         for (int j = 0; j < (i + 1); j++)
         {
@@ -19,7 +21,7 @@ int main()
             do
             {
                 count = 0;
-                for (int k = 0; k < i - count2; k++)
+                for (int k = 0; k < i - count2; k++)      //TODO: Sorting array
                 {
                     if (arr[k] < arr[k + 1])
                     {
@@ -33,7 +35,7 @@ int main()
             }
             while(count > 0);
         }
-        if (i % 2 == 0)
+        if (i % 2 == 0)                                //TODO: Finding median
         {
             int num = i / 2;
             med = (float)arr[num];
@@ -41,9 +43,9 @@ int main()
         else
         {
             int num1 = (i - 1) / 2;
-            med = (arr[num1] + arr[num1 + 1]) / 2.0;
+            med = (arr[num1] + arr[num1 + 1]) / 2.0;   
         }
-        printf("%.1f\n",med);
+        printf("%.1f\n",med);                          //TODO: Printing median
     }
 
 }
