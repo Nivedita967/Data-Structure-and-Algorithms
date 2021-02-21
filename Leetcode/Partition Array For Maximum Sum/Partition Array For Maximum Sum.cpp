0a09d99596bbe27d6de39fa75a961f7914799b80
Jindalh2 <jindalh2@gmail.com>
2021-02-21 15:15:27 +0530

    Update Partition Array For Maximum Sum.cpp

--- Leetcode/Partition Array For Maximum Sum/Partition Array For Maximum Sum.cpp
+++ Leetcode/Partition Array For Maximum Sum/Partition Array For Maximum Sum.cpp
@@ -1,3 +1,16 @@
+
+/*make one array dp of length same as A, and fill this with 0
+for i in range 0 to length of A - 1
+dp[i] = A[i] + dp[i - 1] if i – 1 >= 0 otherwise 0
+temp := A[i]
+for j in range 1 to k – 1
+if i – j >= 0
+index := i – j
+temp := max of temp and A[i - j]
+if index – 1 >= 0, then
+dp[i] := max of dp[i] and (temp*(i – index + 1) + dp[index - 1])
+otherwise dp[i] := max of dp[i] and 0
+return last element of dp*/
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& A, int K) {
