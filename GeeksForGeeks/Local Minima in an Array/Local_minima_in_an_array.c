71128e84388ccfd1f9a042b6c76dd263ab77303d
ishikabansal04 <ikabansal04@gmail.com>
2021-02-26 11:40:28 +0530

    Adding comments for better understanding

--- GeeksForGeeks/Local Minima in an Array/Local_minima_in_an_array.c
+++ GeeksForGeeks/Local Minima in an Array/Local_minima_in_an_array.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 
 int main(){
+
+    //taking input
     int n;
     scanf("%d", &n);
     int arr[100000];
@@ -8,16 +10,25 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
+    //storing index
     int idx=-1;
+
+    //if size of array is only 1 then return idx 0
     if(n==1){
         idx=0;
     }
+
+    //if first index is local minima
     else if(arr[1]>=arr[0]){
         idx=0;
     }
+
+    //if last index is local minima
     else if(arr[n-2]>=arr[n-1]){
         idx=n-1;
     }
+
+    //if local minima lies between 1 to n
     else{
         for(int i=1;i<n-1;i++){
             if(arr[i-1]>= arr[i] && arr[i+1]>=arr[i]){
@@ -26,5 +37,7 @@ int main(){
             }
         }
     }
+
+    //printing local minima
     printf("%d", idx);
 }
