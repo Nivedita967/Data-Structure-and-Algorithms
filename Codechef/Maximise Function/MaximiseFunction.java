3b2cb7f9278fbf2b4255f2a72df93199ece9a5a8
Zenil Haria <zenilharia06@gmail.com>
2021-02-26 21:48:03 +0530

    Added comments.

--- Codechef/Maximise Function/MaximiseFunction.java
+++ Codechef/Maximise Function/MaximiseFunction.java
@@ -1,6 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+/*
+* Problem Name: Maximise Function
+* Problem URL: https://www.codechef.com/FEB21B/problems/MAXFUN
+* Logic: If the array is sorted in ascending order, and we choose the three numbers x,y,z such that x < y < z and Ax <= Ay <= Az
+* then |Ax - Ay| + |Ay - Az| + |Az - Ax| = (Ay - Ax) + (Az - Ay) + (Az - Ax) = 2*(Az - Ax).
+* Hence the objective is to maximize (Az - Ax) such that x < z and Ax <= Az. Hence the solution is the difference between maximum
+* and minimum of the array.
+*
+* Time Complexity - O(n)
+* Space Complexity - O(n)
+*/
+
 public class MaximiseFunction
 {
     public static void main(String[] args)
@@ -23,14 +35,15 @@ public class MaximiseFunction
     }
 
     public long solve(int N, long[] A) {
-        long max = Long.MIN_VALUE;
-        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE; // Setting the initial max value as lowest long value
+        long min = Long.MAX_VALUE; // Setting the initial min value as largest long value
 
         for(long a : A) {
+            // Updating max and min using Math Library
             max = Math.max(max, a);
             min = Math.min(min, a);
         }
 
-        return 2*(max-min);
+        return 2*(max-min); // Returning the answer.
     }
 }
