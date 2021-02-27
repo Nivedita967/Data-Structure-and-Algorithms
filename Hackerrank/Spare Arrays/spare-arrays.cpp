b8edf244a7a0e05c8f01990fc5e1e709e7294423
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-02-28 02:06:33 +0530

    Create spare-arrays.cpp

---
+++ Hackerrank/Spare Arrays/spare-arrays.cpp
@@ -0,0 +1,64 @@
+// https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    vector<int> vRet;
+    for(auto it : queries) {
+        int iCount = count_if(strings.begin(), strings.end(), [&](string s) {
+            return it == s;
+        });
+        vRet.push_back(iCount);
+    }
+    return vRet;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
