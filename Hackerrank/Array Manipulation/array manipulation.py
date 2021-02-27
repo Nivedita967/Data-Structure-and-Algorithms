826e43fb554b41efd6b5ce3ae50160bd3fc0f9b5
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-02-27 23:04:39 +0530

    Update array manipulation.py

--- Hackerrank/Array Manipulation/array manipulation.py
+++ Hackerrank/Array Manipulation/array manipulation.py
@@ -1,12 +1,54 @@
-n,m=map(int,raw_input().split())
-ch=[0 for _ in xrange(n+2)]
-for _ in xrange(m):
-    a,b,k=map(int,raw_input().split())
-    ch[a]+=k
-    ch[b+1]-=k
-v=0
-m=0
-for i in xrange(1,n+1):
-    v+=ch[i]
-    m=max(m,v)
-print m
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def arrayManipulation(n, queries):
+    diffs = getArrayOfDiffs(n, queries)
+    return maxFromDiffs(diffs)
+
+
+def maxFromDiffs(diffs):
+    sumSoFar = 0
+    maxSoFar = 0
+    for diff in diffs:
+        sumSoFar += diff
+        if sumSoFar > maxSoFar:
+            maxSoFar = sumSoFar
+    return maxSoFar
+
+
+def getArrayOfDiffs(n, queries):
+    # An array used to capture the difference of an element
+    # compared to the previous element.
+    # Therefore the value of diffs[n] after all array manipulations is
+    # the cumulative sum of values from diffs[0] to diffs[n - 1]
+    diffs = [0] * n
+    for a, b, k in queries:
+        # Adds "k" to all subsequent elements in the array
+        diffs[a - 1] += k
+        # Ignore if b is out of range
+        if (b < n):
+            # Subtracts "k" from all subsequent elements in the array
+            diffs[b] -= k
+
+    return diffs
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    nm = input().split()
+    n = int(nm[0])
+    m = int(nm[1])
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+    fptr.write(str(result) + '\n')
+    fptr.close()
