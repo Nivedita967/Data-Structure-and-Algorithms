e5a1590e025943bbf0f72f16e972714ca14278db
Priya Nagda <pri1311@users.noreply.github.com>
2021-03-08 16:04:47 +0530

    comments added to sliding window median

--- Leetcode/Sliding Window Median/sliding window median.cpp
+++ Leetcode/Sliding Window Median/sliding window median.cpp
@@ -3,13 +3,13 @@ public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         int a=0;
         int j=0;
-        vector <double> res;
-        multiset <long long int, greater <int>> temp;
-        multiset <long long int, greater <int> > :: iterator itr;
-        for (int i=0;i<nums.size();i++){
+        vector <double> res; // Declare result vector
+        multiset <long long int, greater <int>> temp; //Multisets are a type of associative containers similar to set, with an exception that multiple elements can have same values.
+        multiset <long long int, greater <int> > :: iterator itr; // declare iterator for the declared multiset to iterate through the data structure.
+        for (int i=0;i<nums.size();i++){//iterate through provided vector.
             temp.insert(nums[i]);
             j++;
-            if (j==k){
+            if (j==k){ // when j reaches the window size.
                 itr=temp.begin();
                 advance(itr, temp.size() / 2);
 
