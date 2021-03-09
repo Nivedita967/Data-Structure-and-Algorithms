3b1638191c59cc59f9205c25c0da0ab6677f29ad
arbazalam <drarbazalam@gmail.com>
2021-03-09 12:22:13 +0530

    Added comments for beginners

--- Codechef/Carvans/Carvans.py
+++ Codechef/Carvans/Carvans.py
@@ -1,12 +1,15 @@
-test = int(input())
+# We have to count the number of cars which were moving at their maximum speed on the straight segment.
+
+
+test = int(input())       # input test cases
 for t in range(test):
-    n = int(input())
+    n = int(input())      # taking the number of cars
     speed = list(map(int,input().split()))
     count = 1
     prev_car_speed = speed[0]
-    for i in range(1, n):
+    for i in range(1, n):         # Calculating the number of cars at their maximum speed
         car_speed = speed[i]
         if prev_car_speed >= car_speed:
             prev_car_speed = car_speed
             count += 1
-    print(count)
+    print(count)                  # Printing the result
