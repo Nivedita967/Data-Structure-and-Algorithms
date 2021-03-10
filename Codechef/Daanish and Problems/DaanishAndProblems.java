f2f2c77b2482462ec379c3de5ebf9de31368217a
Simonne Letetia Pinto <53074235+simonnepinto@users.noreply.github.com>
2021-03-10 14:03:01 +0530

    Update DaanishAndProblems.java

--- Codechef/Daanish and Problems/DaanishAndProblems.java
+++ Codechef/Daanish and Problems/DaanishAndProblems.java
@@ -10,21 +10,22 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-	    Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		
+	    	Scanner scan = new Scanner(System.in); 
+		int n = scan.nextInt(); //input the number of test cases
+
 		for(int i=0 ;i<n; i++){
-		    int a[] = new int[n];
+		    int a[] = new int[n]; // array initialisation
 		    for(int j=0; j<10; j++)
-		        a[j] = scan.nextInt();
-		    int k = scan.nextInt();
-		    
+			a[j] = scan.nextInt(); //array of integers
+			
+		    int k = scan.nextInt(); //the number of problems to be deleted
+
 		    int j=9;
 		    while(k >= a[j]){
-		        k = k - a[j];
-		        j--;
+			k = k - a[j];
+			j--; //obtain the minimum difficulty of the most difficult remaining problem.
 		    }
-		    System.out.println(j+1);
+		    System.out.println(j+1); //Print the result
 		}
 	}
 }
