71e1f4c115852fd319e2c07393e9d6eb6cc9e92c
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2021-03-13 03:05:34 -0800

    Delete Arithmetic Slices.py

--- Leetcode/Arithmetic Slices/Arithmetic Slices.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        m = [0]*len(A)  # Creating array consisting of 0's
-        for i in range(0,len(A)-2):
-            if (A[i]-A[i+1])==(A[i+1]-A[i+2]):  # comparing difference between any two consecutive elements
-                m[i+2] = m[i+1]+1
-        return sum(m)
