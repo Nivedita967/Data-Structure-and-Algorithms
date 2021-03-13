f71059b51bae1d9edd13e45092948905f6ac7805
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2021-03-13 08:55:48 +0530

    Create Sum and Difference of Two Numbers.py

---
+++ Hackerrank/Sum and Difference of Two Numbers/Sum and Difference of Two Numbers.py
@@ -0,0 +1,4 @@
+a ,b = map(int,input().split())    # taking user input of int datatype
+c,d =  map(float,input().split())   # taking user input of float datatype
+print(a+b , a-b)                    # addition & subraction of int varibles
+print(c+d , c-d)                    # addition & subraction of float varibles
