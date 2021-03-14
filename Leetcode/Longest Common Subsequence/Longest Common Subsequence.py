e7f3ad4eb981be7e27d805bb4d6f5ec0a137bacf
Abhinav <agrawal.abhinav95@gmail.com>
2021-03-14 14:30:23 +0530

    Added source code for longest common subsequence in python

---
+++ Leetcode/Longest Common Subsequence/Longest Common Subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for i in range(len(text2) + 1)] for j in range(len(text1) + 1)]
+        
+        text1 = " " + text1
+        text2 = " " + text2
+        
+        for i in range(1, len(text1)):
+            for j in range(1, len(text2)):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
+        return dp[-1][-1]
