5f4ef0da924d1053dc3d17e7fd8bcf3712b1c66f
Chirag Agrawal <78378530+chiragagrawal9200@users.noreply.github.com>
2021-03-15 22:19:39 +0530

    Update sum of elements in an array.cpp

--- Hackerrank/sum of elements in the array/sum of elements in an array.cpp
+++ Hackerrank/sum of elements in the array/sum of elements in an array.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main ()
 {
-    int arr[1000], n, i, sum = 0, pro = 1;
+    int arr[1000], n, i, sum = 0, pro = 1;      //initialising array and other variables
     cout << "";
     cin >> n;
     cout << "";
-    for (i = 0; i < n; i++)
+    for (i = 0; i < n; i++)          //input of elements of array using for loop
     cin >> arr[i];
     for (i = 0; i < n; i++)
     {
-        sum += arr[i];
+        sum += arr[i];            //addition of elements 0f array
     }
     cout << "" << sum;
     return 0;
