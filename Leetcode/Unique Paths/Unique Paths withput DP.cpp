5dff12e9158b36023a2c1b1a6eca56c92638ee5f
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-03-16 23:16:51 +0530

    Unique Paths done without using DP.

---
+++ Leetcode/Unique Paths/Unique Paths withput DP.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  // Efficient code to Calculate nCr Combinations
+  int NcR(int n, int r) {
+    long long p = 1, k = 1;
+    if (n - r < r)
+      r = n - r;
+    if (r != 0) {
+      while (r) {
+        p *= n;
+        k *= r;
+        long long m = __gcd(p, k);
+        p /= m;
+        k /= m;
+        n--;
+        r--;
+      }
+    } else
+      p = 1;
+    return p;
+  }
+  int uniquePaths(int m, int n) {
+    // The solution works using (m+n)C(m) [Combinations from pascal's Triangle]
+    return NcR(m + n - 2, m - 1);
+  }
+};
