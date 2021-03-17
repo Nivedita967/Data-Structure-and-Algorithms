bbf3b37928a7152b0f71b8cf3a14f1f35750d3ec
Abhinav <agrawal.abhinav95@gmail.com>
2021-03-17 11:11:23 +0530

    Added comments for easier understanding

--- Leetcode/Path with Maximum Gold/Path with Maximum Gold.py
+++ Leetcode/Path with Maximum Gold/Path with Maximum Gold.py
@@ -40,15 +40,18 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        # nRow will be storing the number of rows in the grid & nCol is the number of columns in the grid
         nRow, nCol = len(grid), len(grid[0])
         res = 0
+        # Traverse each elemet of the grid
         for r in range(nRow):
-            for c in range(nCol):
-                if grid[r][c] == 0: continue
-                stack = [(r, c, {(r, c)}, grid[r][c])]
+            for c in range(nCol): 
+                if grid[r][c] == 0: continue # if the element is not 0 then only perform DFS
+                stack = [(r, c, {(r, c)}, grid[r][c])] # Create a stack of currentRow r, currentColumn c, {(r,c)} is the corordinate & grid[r][c] is the value of elemet
                 while stack:
-                    x, y, path, d = stack.pop()
-                    res = max(res, d)
+                    x, y, path, d = stack.pop() # pop elements and put it in respective variables
+                    res = max(res, d) 
+                    # [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)] == [(previous row, currentCol),(nextRow,currentCol),(currentRow,previous Column), (currentRow, nextCol)]
                     for x1, y1 in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
                         if 0 <= x1 < nRow and 0 <= y1 < nCol and (x1, y1) not in path and grid[x1][y1] != 0:
                             stack += [(x1, y1, path | {(x1, y1)}, d + grid[x1][y1])]
