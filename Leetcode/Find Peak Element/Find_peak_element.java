d54886f282c8be0279c2fa3a624a6c835f616276
Amit kumar mishra <akm43300@gmail.com>
2021-03-19 20:23:18 +0530

    Find peak element

--- Leetcode/Find Peak Element/Find_peak_element.java
+++ Leetcode/Find Peak Element/Find_peak_element.java
@@ -33,7 +33,6 @@ public class Find_peak_element {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int a[] = new int[n];
-        
         for(int i=0;i<n;i++)
           a[i]=sc.nextInt();
 
