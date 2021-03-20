614526aff729f6fadfc7ca0ad3f7f206ed9c046f
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-03-20 11:02:05 +0530

    Update vaccine production.py

--- Codechef/Vaccine Production/vaccine production.py
+++ Codechef/Vaccine Production/vaccine production.py
@@ -1,6 +1,14 @@
+// A Program in Python to analise production of vaccine
+
+
+// Reading input
+
 d1,v1,d2,v2,x = map(int,input().split())
 total = 0
 ans = 0
+
+// Calculating Production
+
 while total < x:
     if d1 == 1:
         total += v1
@@ -11,4 +19,7 @@ while total < x:
     else:
         d2 -= 1
     ans += 1
+    
+//Printing Result    
+
 print(ans)
