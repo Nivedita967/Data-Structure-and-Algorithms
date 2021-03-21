238ef6f9d59dfb6ebde2bfc5a1c472acdfbabcea
Amit kumar mishra <akm43300@gmail.com>
2021-03-21 21:21:48 +0530

    CuttingBoard

--- Hackerrank/Cutting Board/CuttingBoard.java
+++ Hackerrank/Cutting Board/CuttingBoard.java
@@ -3,7 +3,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Boardcutting {
+public class CuttingBoard {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
