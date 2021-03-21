b1b6ce205912d326c13fd84d7318d2d78276ec22
Priya Nagda <pri1311@users.noreply.github.com>
2021-03-22 01:05:22 +0530

    BitwiseOperators

--- Hackerrank/Bitwise Operators/Bitwise Operators.cpp
+++ Hackerrank/Bitwise Operators/Bitwise Operators.cpp
@@ -2,18 +2,22 @@
 using namespace std;
  
 int main() {
+
+    //code snippet for fast i/o
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
+
+    
     int n,k;
     cin>>n>>k;
     
-    int a,b,c=0,d=0,e=0;
+    int a,b,c=0,d=0,e=0; //c d and e will store and or and xor results repectively.
     
-    for (a=1;a<n;a++)
+    for (a=1;a<n;a++)  //iterate from start
     {
-        for (b=n;b>a;b--)
+        for (b=n;b>a;b--) //iterate from end
         {
-            if ((a&b)<k && (a&b)>c) {c = a&b;}
+            if ((a&b)<k && (a&b)>c) {c = a&b;} 
             if ((a|b)<k && (a|b)>d) {d = a|b;}
             if ((a^b)<k && (a^b)>e) {e = a^b;}
         }
