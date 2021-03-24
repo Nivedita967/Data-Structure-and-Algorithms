611471525231e744d9535d579ef7466d7e809dc5
Abhinav <agrawal.abhinav95@gmail.com>
2021-03-24 22:13:29 +0530

    Updated folder name

--- Hackerrank/Recursive digit sum/Recursive digit sum.java
+++ Hackerrank/Recursive digit sum/Recursive digit sum.java
@@ -1,7 +1,7 @@
 // We define super digit of an integer x  using the following rules:
 // Given an integer, we need to find the super digit of the integer.
 // -> If x  has only  digit, then its super digit is x.
-// -> Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x.
+// -> Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x
 import java.util.*;
 
 
