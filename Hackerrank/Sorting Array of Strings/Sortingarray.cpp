aab2b55e33942e544ba23cccdda8dee03b61b9c0
Amit kumar mishra <akm43300@gmail.com>
2021-03-24 12:05:34 +0530

    Sorted string

---
+++ Hackerrank/Sorting Array of Strings/Sortingarray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+    int n;
+    cin>>n;
+    string str[n], temp;
+
+    cout << "Enter words: " << endl;
+    for(int i = 0; i < n; ++i)
+    {
+      getline(cin, str[i]);
+    }
+
+    // Use Bubble Sort to arrange words
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = 0; j < n-1 - i; ++j) {
+            if (str[j] > str[j + 1]) {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "In lexicographical order: " << endl;
+
+    for(int i = 0; i < n; ++i)
+    {
+       cout << str[i] << endl;
+    }
+    return 0;
+}
+
