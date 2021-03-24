39431a592d5f6e0c317a8bf0879181b3d262ea3c
Abhinav <agrawal.abhinav95@gmail.com>
2021-03-24 16:52:35 +0530

    Added source code for 'Pascal Triangle' in Java

---
+++ Leetcode/Pascal's Triangle/Pascal's Triangle.java
@@ -0,0 +1,37 @@
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+        // First base case; if user requests zero rows, they get zero rows.
+        if (numRows == 0) {
+            return triangle;
+        }
+
+        // Second base case; first row is always [1].
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum-1);
+
+            // The first row element is always 1.
+            row.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+
+            // The last row element is always 1.
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
