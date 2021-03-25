f95f4844f392d180015b931bed20eef2420b3714
Yashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-03-25 07:28:25 +0530

    Update Validate Stack Sequences.py

--- Leetcode/Validate Stack Sequences/Validate Stack Sequences.py
+++ Leetcode/Validate Stack Sequences/Validate Stack Sequences.py
@@ -1,14 +1,14 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        Stack=[]                                     # Creaating a stack
-        pushed=deque(pushed)
-        popped=deque(popped)
+        Stack=[]                                     # Creaating an empty stack
+        pushed=deque(pushed)                         # Creating double ended queue
+        popped=deque(popped)                         
         while(len(pushed)):
             
             if len(Stack)==0:
                 Stack.append(pushed.popleft())
             else:
-                push = pushed.popleft()
+                push = pushed.popleft()              # Pushing and popping according to input
                 pop  = popped.popleft()
                 if Stack[-1]==pop:
                     Stack.pop()
@@ -17,7 +17,7 @@ class Solution:
                     Stack.append(push)
                     popped.appendleft(pop)
         
-        while(len(Stack)):                            # Updating stack
+        while(len(Stack)):                            # Checking whether those input can be used in a stack (if we finally get an empty stack, input is valid)
             pop=popped.popleft()
             top=Stack.pop()
             if pop!=top:
