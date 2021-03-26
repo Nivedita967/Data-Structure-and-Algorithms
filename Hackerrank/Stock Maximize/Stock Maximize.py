48e3738380b7617f31ae90b91edf074c74c17229
Priya Nagda <64613009+pri1311@users.noreply.github.com>
2021-03-26 21:01:11 +0530

    Update Stock Maximize.py

--- Hackerrank/Stock Maximize/Stock Maximize.py
+++ Hackerrank/Stock Maximize/Stock Maximize.py
@@ -1,8 +1,7 @@
-def iter_over_p(p):
+def stockmax(p):
     ind_max = p.index(max(p)) #find the max price
     inv = sum(p[:ind_max]) #split the array before and after max price
     pf = len(p[:ind_max])*p[ind_max] - inv #buy all stocks before max price 
     if len(p[ind_max+1:]) > 0:
-        pf += iter_over_p(p[ind_max+1:]) #then sell them at max price
+        pf += stockmax(p[ind_max+1:]) #then sell them at max price
     return pf
-profit = iter_over_p(prices)
