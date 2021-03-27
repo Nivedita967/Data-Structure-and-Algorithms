ae2460cd517745bd8efb487cdf17238af0d875a3
Abhinav Agrawal <agrawal.abhinav98@gmail.com>
2021-03-27 18:17:46 +0530

    Updated comments

--- Hackerrank/Electronics Shop/Electronics Shop.java
+++ Hackerrank/Electronics Shop/Electronics Shop.java
@@ -1,10 +1,8 @@
 //Problem: https://www.hackerrank.com/challenges/electronics-shop
 /* 
-
-
-
-
-
+Problem Statement:
+A person wants to determine the most expensive computer keyboard and USB drive that can be purchased with a give budget.
+Given price lists for keyboards and USB drives and a budget, find the cost to buy them. If it is not possible to buy both items, -1 return .
 */
 import java.io.*;
 import java.util.*;
