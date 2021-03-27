cb1e5f63de93ce11ab5f598b889301b61455d553
ishikabansal04 <ikabansal04@gmail.com>
2021-03-27 08:57:48 +0530

    Adding source code for Perfect Squares in Java

---
+++ Leetcode/Perfect Squares/PerfectSquares.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        // initialising dp[0] & dp[1];
+        dp[0]=0;
+        dp[1]=1;
+
+        // from i >=2 we look back for all perfect squares and store min value
+        for(int i=2; i<=n; i++){
+            for(int j=1; j*j <=i; j++){
+                dp[i]= Math.min(dp[i], dp[i-(j*j)]+1);
+            }
+        }
+
+        //returning dp[n]
+        return dp[n];
+    }
+}
