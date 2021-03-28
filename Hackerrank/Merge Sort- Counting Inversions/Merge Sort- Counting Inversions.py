54f90de08bd53492528b43bc29df2a0adac3213d
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2021-03-28 09:25:53 -0700

    Update Merge Sort- Counting Inversions.py

--- Hackerrank/Merge Sort- Counting Inversions/Merge Sort- Counting Inversions.py
+++ Hackerrank/Merge Sort- Counting Inversions/Merge Sort- Counting Inversions.py
@@ -10,17 +10,17 @@ def countInversions(array):
 
     if len(array) <= 1:
         return array, 0
-
-    ar_left, inv_left = countInversions(array[:int(len(array)/2)])
-    ar_right, inv_right = countInversions(array[int(len(array)/2):])
+    # break an array in two halfs
+    ar_left, inv_left = countInversions(array[:int(len(array)/2)])   # first half of the array
+    ar_right, inv_right = countInversions(array[int(len(array)/2):])  # second half of the array
 
     ar_merged = []
     i = j = 0
-    len_left = len(ar_left)
-    len_right = len(ar_right)
+    len_left = len(ar_left)  # length of the left half
+    len_right = len(ar_right)  # length of the second half
     
     for k in range(len(array) - 1):
-        if i == len_left or j == len_right:
+        if i == len_left or j == len_right:  # break the iteration when length of left half become equal to i or right half become equal to j
             break
 
         if ar_left[i] <= ar_right[j]:
