434e024da27fff171d22233185dce92afd343730
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-03-28 22:26:04 +0530

    Update InvertBinaryTree.c

--- Leetcode/Invert Binary Tree/InvertBinaryTree.c
+++ Leetcode/Invert Binary Tree/InvertBinaryTree.c
@@ -1,3 +1,9 @@
+//Problem:https://leetcode.com/problems/invert-binary-tree/
+
+//Approach:Main Idea in inverting here is, to attach the left sub tree
+//to the right of root and, attach the right subtree to the left of the root. 
+//Repeat this for each and every node, and eventually the tree will be inverted. 
+
 void preOrder(struct TreeNode* root){
     if(!root)
         return;
